2c109514217ebbca2b0d5c4f9216a5107dc16a66
IamArnab999 <arnabkantdasi2004@gmail.com>
2024-04-29 01:27:53 +0530

    first commit

---
+++ automationbot.py
@@ -0,0 +1,2 @@
+import pywhatkit as pyw
+pyw.sendwhatmsg('+91--phn.number--', 'Hello hello', 22, 33)
---
+++ automationbotpro.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+
+while True:
+    time.sleep(3)
+    pyautogui.typewrite("Good morning")
+    pyautogui.press('enter')
